b1f32548975781e86b5a0a471ee2fb57e2cbd30c
Fahad-smto <simanto.smto.prog@gmail.com>
2026-08-19 23:41:53 +0600

    code

--- model/Photo.ts
+++ model/Photo.ts
@@ -13,4 +13,5 @@ const PhotoSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
 });
 
+// মডেলটি ইতিমধ্যে তৈরি থাকলে পুনরায় তৈরি না করা
 export default mongoose.models.Photo || mongoose.model('Photo', PhotoSchema);
