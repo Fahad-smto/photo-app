044c4f12977bd5737bd870541d7fde57b9eefce7
Fahad-smto <simanto.smto.prog@gmail.com>
2026-08-21 23:41:38 +0600

    code

--- app/components/PhotoGrid.tsx
+++ app/components/PhotoGrid.tsx
@@ -1,12 +1,6 @@
 import ImageCard from "./ImageCard";
 
 
-interface Photo {
-  id: string;
-  url: string;
-  title: string;
-  createdAt: string;
-}
 
 interface PhotoGridProps {
   photos: Photo[];
