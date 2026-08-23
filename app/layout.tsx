c22f570c4c3357f27f8de268bfeca381014cfbc3
Fahad-smto <simanto.smto.prog@gmail.com>
2026-08-23 23:38:00 +0600

    code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Navigation from "./components/Navigation";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
