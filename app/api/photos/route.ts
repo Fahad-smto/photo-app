3bce4680a2cf415b526ec2ec821812b3fa649ade
Fahad-smto <simanto.smto.prog@gmail.com>
2026-08-29 23:48:12 +0600

    code added

--- app/api/photos/route.ts
+++ app/api/photos/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from 'next/server';
 import { connectToDatabase } from '@/lib/mongodb';
 import Photo from '@/model/Photo';
 
-export async function GET() {
   try {
     console.log("1. API called - fetching photos");
     
