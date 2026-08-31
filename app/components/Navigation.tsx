52349d3799fca073e755dc997d4bc13ba01bb0ff
Fahad-smto <simanto.smto.prog@gmail.com>
2026-08-31 23:54:35 +0600

    code added

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -4,6 +4,9 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Camera, Home, Upload } from "lucide-react";
 
+export default function Navigation() {
+  const pathname = usePathname();
+
   return (
     <>
       <style>{`
